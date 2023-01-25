ee05d1c32fe8826e9415c68d73f9a518f765dd8d
Saurabh Pal <118064701+saurabh27pal@users.noreply.github.com>
2023-01-25 12:19:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 function threeSum(arr, target) {
-  //your code here
+  let closestSum = Infinity;
+  let closestTriplet;
+
+  // Sort the array
+  S.sort((a, b) => a - b);
+
+  // Iterate through the array and find the closest sum
+  for (let i = 0; i < S.length - 2; i++) {
+    let left = i + 1;
+    let right = S.length - 1;
+    while (left < right) {
+      let sum = S[i] + S[left] + S[right];
+      if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
+        closestSum = sum;
+        closestTriplet = [S[i], S[left], S[right]];
+      }
+      if (sum < target) {
+        left++;
+      } else {
+        right--;
+      }
+    }
+  }
+  return closestSum;
+
 }
 
 module.exports = threeSum;
